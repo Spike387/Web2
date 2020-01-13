7640153248031172d5a5554aa96a5b389ea35ee7
sb303 <56815068+sb303@users.noreply.github.com>
2020-01-13 08:59:44 +0100

    Add files via upload

--- zusammenfassung.js
+++ zusammenfassung.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     for (var loesche in produkte){
         localStorage.removeItem(produkte[loesche]);
     }
-
+    console.log("Hallo")
     var bestellid = localStorage.getItem("bestellung");
     $.ajax({
         url: "http://localhost:8000/api/bestellung/gib/"+bestellid,
@@ -17,8 +17,31 @@ $(document).ready(function(){
         dataType: "json"
     }).done(function(response){
         console.log(response.daten); // Hier musst du die Darstellung einfügen
-    })
+        $("#123").empty();
+        for (var i = 0; i < response.daten.length; i++) {  
+            { 
+                eintrag = $("<div>");
+                eintrag.prop({class : "row m-3 bg-light border border-primary", style:"height:3.5rem"});
+                prod_text = $("<p>");
+                prod_text.prop("class","col col-md-6 mh-100 overflow-hidden")
+                prod_text.html("<b>"+response.daten[i].bezeichnung);
+                eintrag.append(prod_text);
+
+                prod_menge = $("<p>");
+                prod_menge.prop("class", "col-md-1");
+                prod_menge.html('<b>Menge:'+localStorage.getItem(response.daten[i].bezeichnung).split(";")[1]);            
+                eintrag.append(prod_menge);
+
+                prod_preis = $("<p>");
+                prod_preis.prop("class", "col col-md-1");
+                prod_preis.html("<b>Preis:</b> <br>" + (response.daten[i].bruttopreis*(localStorage.getItem(response.daten[i].bezeichnung).split(";")[1])) + "&euro;");
+                eintrag.append(prod_preis);
+                //div mit id 123 einfügen
+                $("#bestellung").append(eintrag);
+            }
+        }
+
 
     localStorage.removeItem("bestellung");
-})    
- 
+    });    
+});
